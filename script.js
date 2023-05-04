1ce83460009d01335c81b724bc75d1cf61777a91
Elizabeth Sainz <elisainz@yahoo.com.ar>
2023-05-03 23:54:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,20 @@
-const carousel = document.querySelectorAll(".carousel");
+var carousel = $(".carousel"),
+    currdeg  = 0;
 
-carousel.forEach((carousel) => {
-  const figure = carousel.querySelector("figure");
-  const navPrev = carousel.querySelector(".nav.prev");
-  const navNext = carousel.querySelector(".nav.next");
+$(".next").on("click", { d: "n" }, rotate);
+$(".prev").on("click", { d: "p" }, rotate);
 
-  let currentIndex = 0;
-  let targetIndex = 0;
-
-  const slideTo = (index) => {
-    figure.style.transform = `translateX(-${index * 100}%)`;
-    currentIndex = index;
-  };
-
-  const nextSlide = () => {
-    targetIndex = currentIndex + 1;
-
-    if (targetIndex > figure.children.length - 1) {
-      targetIndex = 0;
-    }
-
-    slideTo(targetIndex);
-  };
-
-  const prevSlide = () => {
-    targetIndex = currentIndex - 1;
-
-    if (targetIndex < 0) {
-      targetIndex = figure.children.length - 1;
-    }
-
-    slideTo(targetIndex);
-  };
-
-  navPrev.addEventListener("click", prevSlide);
-  navNext.addEventListener("click", nextSlide);
-});
+function rotate(e){
+  if(e.data.d=="n"){
+    currdeg = currdeg - 60;
+  }
+  if(e.data.d=="p"){
+    currdeg = currdeg + 60;
+  }
+  carousel.css({
+    "-webkit-transform": "rotateY("+currdeg+"deg)",
+    "-moz-transform": "rotateY("+currdeg+"deg)",
+    "-o-transform": "rotateY("+currdeg+"deg)",
+    "transform": "rotateY("+currdeg+"deg)"
+  });
+}
