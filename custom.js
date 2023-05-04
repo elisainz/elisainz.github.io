09bcc2872a8e0f4d606116a4eedb303d3b37a2e1
Elizabeth Sainz <elisainz@yahoo.com.ar>
2023-05-03 23:32:02 -0300

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,69 +1,27 @@
-const carousel = document.querySelector(".carousel");
-const carouselItems = document.querySelector(".carousel-items");
-const carouselItem = document.querySelectorAll(".carousel-item");
-const caseStudyInfo = document.querySelectorAll(".case-study-info");
-let currentIndex = 1;
-let startX = null;
-let endX = null;
+var
+	carousel = document.querySelector('.carousel'),
+	figure = carousel.querySelector('figure'),
+	nav = carousel.querySelector('nav'),
+	numImages = figure.childElementCount,
+	theta =  2 * Math.PI / numImages,
+	currImage = 0
+;
+	
+nav.addEventListener('click', onClick, true);
 
-// Set the second item as the default active item
-carouselItems.style.transform = "translateX(-33.33%)";
-carouselItem[currentIndex].classList.add("active");
-caseStudyInfo[currentIndex].style.display = "block";
-
-// Move to the next slide
-const nextSlide = () => {
-  if (currentIndex >= carouselItem.length - 1) return;
-  carouselItems.style.transform = `translateX(-${(currentIndex + 1) * 33.33}%)`;
-  carouselItem[currentIndex].classList.remove("active");
-  caseStudyInfo[currentIndex].style.display = "none";
-  currentIndex++;
-  carouselItem[currentIndex].classList.add("active");
-  caseStudyInfo[currentIndex].style.display = "block";
-};
-
-// Move to the previous slide
-const prevSlide = () => {
-  if (currentIndex <= 0) return;
-  carouselItems.style.transform = `translateX(-${(currentIndex - 1) * 33.33}%)`;
-  carouselItem[currentIndex].classList.remove("active");
-  caseStudyInfo[currentIndex].style.display = "none";
-  currentIndex--;
-  carouselItem[currentIndex].classList.add("active");
-  caseStudyInfo[currentIndex].style.display = "block";
-};
-
-// Add event listener for touch start event
-carouselItems.addEventListener("touchstart", e => {
-  startX = e.touches[0].clientX;
-});
-
-// Add event listener for touch end event
-carouselItems.addEventListener("touchend", e => {
-  endX = e.changedTouches[0].clientX;
-  const diffX = startX - endX;
-  if (diffX > 0) {
-    nextSlide();
-  } else if (diffX < 0) {
-    prevSlide();
-  }
-});
-
-// Add event listener for mouse down event
-carouselItems.addEventListener("mousedown", e => {
-  startX = e.clientX;
-});
-
-// Add event listener for mouse up event
-carouselItems.addEventListener("mouseup", e => {
-  endX = e.clientX;
-  const diffX = startX - endX;
-  if (diffX > 0) {
-    nextSlide();
-  } else if (diffX < 0) {
-    prevSlide();
-  }
-});
-
-// Remove the navigation buttons
-document.querySelector(".carousel-nav").remove();
+function onClick(e) {
+	e.stopPropagation();
+	
+	var t = e.target;
+	if (t.tagName.toUpperCase() != 'BUTTON')
+		return;
+	
+	if (t.classList.contains('next')) {
+		currImage++;
+	}
+	else {
+		currImage--;
+	}
+	
+	figure.style.transform = `rotateY(${currImage * -theta}rad)`;
+}
