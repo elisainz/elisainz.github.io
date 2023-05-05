3da49709f0756a242cccea3debcbd666a6e818f2
Elizabeth Sainz <elisainz@yahoo.com.ar>
2023-05-05 14:25:34 -0300

    Main changes

--- script.js
+++ script.js
@@ -5,13 +5,16 @@ let maxZIndex = navLinks.length;
 let easeInOutQuart = "cubic-bezier(0.77, 0, 0.175, 1)";
 slides[0].classList.add("active");
 navLinks[0].classList.add("active");
-navLinks.forEach((navLink, activeIndex) => {
-  (overlays[activeIndex] as HTMLElement).style.zIndex = `${navLinks.length -
-    activeIndex}`;
-  navLink.addEventListener("click", () => {
+
+navLinks.forEach(function(navLink, activeIndex) {
+  overlays[activeIndex].style.zIndex = `${navLinks.length - activeIndex}`;
+  navLink.addEventListener("click", function() {
     // nav-link
-    navLinks.forEach(navLink => navLink.classList.remove("active"));
+    navLinks.forEach(function(navLink) {
+      navLink.classList.remove("active");
+    });
     navLink.classList.add("active");
+
     // slide
     let currentSlide = document.querySelector(".carousel .slides img.active");
     let slideFadeOut = currentSlide.animate(
@@ -25,8 +28,10 @@ navLinks.forEach((navLink, activeIndex) => {
         fill: "forwards"
       }
     );
-    slideFadeOut.onfinish = () => {
-      slides.forEach(slide => slide.classList.remove("active"));
+    slideFadeOut.onfinish = function() {
+      slides.forEach(function(slide) {
+        slide.classList.remove("active");
+      });
       let activeSlide = slides[activeIndex];
       activeSlide.classList.add("active");
       activeSlide.animate(
@@ -43,10 +48,11 @@ navLinks.forEach((navLink, activeIndex) => {
         { duration: 600, easing: "ease-out", fill: "forwards" }
       );
     };
+
     // overlay
     maxZIndex += 1;
     let activeOverlay = overlays[activeIndex];
-    (activeOverlay as HTMLElement).style.zIndex = `${maxZIndex}`;
+    activeOverlay.style.zIndex = `${maxZIndex}`;
     activeOverlay.animate(
       [{ transform: "scaleX(0)" }, { transform: "scaleX(1)" }],
       { duration: 1200, fill: "forwards", easing: easeInOutQuart }
